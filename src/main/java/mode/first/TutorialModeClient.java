eb38593df06dc03745324dc6c98b2dd61f426361
admin <eternity_cr9p@mail.ru>
2023-12-20 11:32:57 +0300

    Initial commit

---
+++ src/main/java/mode/first/TutorialModeClient.java
@@ -0,0 +1,10 @@
+package mode.first;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/mode/first/TutorialModeDataGenerator.java
@@ -0,0 +1,11 @@
+package mode.first;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialModeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

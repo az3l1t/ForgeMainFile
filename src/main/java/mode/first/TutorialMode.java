1e1f5342898d8e23cbffc81a5c58430dca5f7bd9
admin <eternity_cr9p@mail.ru>
2023-12-20 11:39:31 +0300

    some changes!

--- src/main/java/mode/first/TutorialMode.java
+++ src/main/java/mode/first/TutorialMode.java
@@ -11,7 +11,7 @@ public class TutorialMode implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "tutorialmode";
     public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmode");
-
+//some example code
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
